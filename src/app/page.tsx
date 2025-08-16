71170f1109eea4b6cf57d0cb0fe72b1201cd82f7
thatiuday <thatiuday2004@gmail.com>
2025-08-16 21:17:50 +0530

    added how it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Hero from "../app/landing/components/Hero";
+import How from "../app/landing/components/How";
+
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <How />
     </div>
   );
 }
